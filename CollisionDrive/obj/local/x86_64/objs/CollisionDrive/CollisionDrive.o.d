1e443d7dd7e3b810878823fda65a3512dfd18669
Shaji Khan <git@shaji.in>
2023-04-21 13:15:09 +0530

    ndk build

---
+++ CollisionDrive/obj/local/x86_64/objs/CollisionDrive/CollisionDrive.o.d
@@ -0,0 +1,23 @@
+./obj/local/x86_64/objs/CollisionDrive/CollisionDrive.o: \
+  jni/CollisionDrive.cpp jni/resampler.cc jni/zita-resampler/resampler.h \
+  jni/zita-resampler/resampler-table.h jni/resampler-table.cc \
+  jni/gx_resampler.cc jni/gx_resampler.h jni/CollisionDrive.cc \
+  jni/ts9nonlin.h jni/ts9nonlin.cc
+
+jni/resampler.cc:
+
+jni/zita-resampler/resampler.h:
+
+jni/zita-resampler/resampler-table.h:
+
+jni/resampler-table.cc:
+
+jni/gx_resampler.cc:
+
+jni/gx_resampler.h:
+
+jni/CollisionDrive.cc:
+
+jni/ts9nonlin.h:
+
+jni/ts9nonlin.cc:
